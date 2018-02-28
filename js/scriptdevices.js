fa643eee44849d3e3903866e9e7e6d59dda475a8
Teo-Reat <teo.reat@gmail.com>
2018-02-28 15:45:53 +0200

    Add popup window (login), begin work with JS

--- js/scriptdevices.js
+++ js/scriptdevices.js
@@ -1,2 +1,24 @@
-// 11:00 наше время.
-// 21:00 их время.
+var link = document.querySelector('.login-but');
+var popupLogin = document.querySelector('.write-us-popup');
+var overlay = document.querySelector('.overlay');
+
+link.addEventListener('click', function (event) {
+    event.preventDefault();
+    popupLogin.classList.add('modal-content-show');
+    overlay.classList.add('modal-content-show');
+});
+
+overlay.addEventListener('click', function (event) {
+    event.preventDefault();
+    overlay.classList.remove('modal-content-show');
+    popupLogin.classList.remove('modal-content-show');
+});
+
+window.addEventListener('keydown', function (event) {
+    if (event.keyCode === 27) {
+        if (popupLogin.classList.contains('modal-content-show')) {
+            popupLogin.classList.remove('modal-content-show');
+            overlay.classList.remove('modal-content-show');
+        }
+    }
+};
