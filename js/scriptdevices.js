3b9a3ee8dbcaf3a5147070ce899e25aea6d1e58c
Teo-Reat <teo.reat@gmail.com>
2018-03-03 14:38:50 +0200

    Redesign of the site, added a search field

--- js/scriptdevices.js
+++ js/scriptdevices.js
@@ -1,5 +1,6 @@
 var link = document.querySelector('.login-but');
 var link2 = document.querySelector('.login-but-js');
+var login = document.querySelector('[name = login]');
 var popupLogin = document.querySelector('.write-us-popup');
 var closeButton = document.querySelector('.close-button');
 var overlay = document.querySelector('.overlay');
@@ -12,7 +13,7 @@ var buttonOnSlide21 = document.querySelector('.but-slide-2-1');
 var buttonOnSlide23 = document.querySelector('.but-slide-2-3');
 var buttonOnSlide31 = document.querySelector('.but-slide-3-1');
 var buttonOnSlide32 = document.querySelector('.but-slide-3-2');
-var butMenuCatalog = document.querySelector('.catalog');
+var butMenuCatalog = document.querySelector('.catalog-link');
 var catalogMenu = document.querySelector('.catalog-menu');
 var servicesMenuDelivery = document.querySelector('.services-menu01');
 var servicesMenuWarranty = document.querySelector('.services-menu02');
@@ -20,12 +21,17 @@ var servicesMenuCredit = document.querySelector('.services-menu03');
 var servicesSlideDelivery = document.querySelector('.delivery-block');
 var servicesSlideWarranty = document.querySelector('.warranty-block');
 var servicesSlideCredit = document.querySelector('.credit-block');
+var searchBlock = document.querySelector('.search-block');
+var searchButton = document.querySelector('.search');
+var searchLink = document.querySelector('.search-link');
+var searchField = document.querySelector('.input-search');
 
-// Окно при нажатии на кнопку "Login"
+// Pop-up window when you click on button "Login"
 link.addEventListener('click', function (event) {
     event.preventDefault();
     popupLogin.classList.add('modal-content-show');
     overlay.classList.add('modal-content-show');
+    login.focus();
 });
 
 link2.addEventListener('click', function (event) {
@@ -34,7 +40,7 @@ link2.addEventListener('click', function (event) {
     overlay.classList.add('modal-content-show');
 });
 
-// Закрытие окна входа
+// Close the "Login" window
 overlay.addEventListener('click', function (event) {
     event.preventDefault();
     overlay.classList.remove('modal-content-show');
@@ -55,7 +61,7 @@ window.addEventListener('keydown', function (event) {
     }
 });
 
-// Переключение слайдов
+// Switching slides
 buttonOnSlide12.addEventListener('click', function () {
     if (slide01.classList.contains('modal-content-show')) {
         slide01.classList.remove('modal-content-show');
@@ -110,7 +116,7 @@ buttonOnSlide32.addEventListener('click', function () {
     slide02.classList.add('modal-content-show');
 });
 
-// Раскрытие верхнего меню при наведении
+// Opening the top menu when hovering
 butMenuCatalog.addEventListener('mouseover', function (event) {
     event.preventDefault();
     catalogMenu.classList.remove('modal-content-hide');
@@ -123,7 +129,7 @@ butMenuCatalog.addEventListener('mouseout', function (event) {
     catalogMenu.classList.remove('modal-content-show');
 });
 
-// Переключение слайдов 2
+// Switching slides 2
 servicesMenuDelivery.addEventListener('click', function (event) {
     event.preventDefault();
     servicesSlideDelivery.classList.remove('slide-hide');
@@ -143,4 +149,19 @@ servicesMenuCredit.addEventListener('click', function (event) {
     servicesSlideCredit.classList.remove('slide-hide');
     servicesSlideWarranty.classList.add('slide-hide');
     servicesSlideDelivery.classList.add('slide-hide');
+});
+
+// Pop-up search box
+searchButton.addEventListener('click', function (event) {
+    event.preventDefault();
+    searchBlock.classList.remove('slide-hide');
+    searchLink.classList.add('modal-content-hide');
+    searchField.focus();
+});
+
+window.addEventListener('keydown', function (event) {
+    if (event.keyCode === 27) {
+        searchBlock.classList.add('slide-hide');
+        searchLink.classList.remove('modal-content-hide');
+    }
 });
